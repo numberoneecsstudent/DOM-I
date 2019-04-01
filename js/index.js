2db41ad126073c34f474e41bd3db87a24a1eeb88
numberoneecsstudent <32531002+numberoneecsstudent@users.noreply.github.com>
2019-04-01 13:47:28 -0700

    finished the project

--- js/index.js
+++ js/index.js
@@ -50,7 +50,7 @@ logo.setAttribute('src', siteContent["nav"]["img-src"])
 
 
 //nav
-const nav = document.querySelector('a')
+const nav = document.querySelectorAll('a');
 nav[0].textContent = siteContent['nav']['nav-item-1'];
 nav[1].textContent = siteContent['nav']['nav-item-2'];
 nav[2].textContent = siteContent['nav']['nav-item-3'];
@@ -69,29 +69,29 @@ nav[5].textContent = siteContent['nav']['nav-item-6'];
 //===============================================================================
 // cta
 
-const cta_text = document.querySelector('h1')
+const cta_text = document.querySelector('h1');
 cta_text.textContent = siteContent['cta']['h1'];
 
-const button = document.querySelector('button')
+const button = document.querySelector('button');
 button.textContent = siteContent['cta']['button'];
 
-const cta_img = document.getElementById('cta-img')
+const cta_img = document.getElementById('cta-img');
 cta_img.setAttribute("src", siteContent["cta"]["img-src"]);
 
 
-const main_content_img = document.getElementById('middle-img')
+const main_content_img = document.getElementById('middle-img');
 main_content_img.setAttribute("src", "img/mid-page-accent.jpg")
 
 // main-content
 
-const main_content_h4 = document.querySelector('.main content h4')
+const main_content_h4 = document.querySelectorAll('h4');
 main_content_h4[0].textContent = siteContent['main-content']['features-h4'];
 main_content_h4[1].textContent = siteContent['main-content']['about-h4'];
 main_content_h4[2].textContent = siteContent['main-content']['services-h4'];
 main_content_h4[3].textContent = siteContent['main-content']['product-h4'];
 main_content_h4[4].textContent = siteContent['main-content']['vision-h4'];
 
-const main_content_p = document.querySelector('.main-content p')
+const main_content_p = document.querySelectorAll('p');
 main_content_p[0].textContent = siteContent['main-content']['features-content'];
 main_content_p[1].textContent = siteContent['main-content']['about-content'];
 main_content_p[2].textContent = siteContent['main-content']['services-content'];
@@ -100,28 +100,23 @@ main_content_p[4].textContent = siteContent['main-content']['vision-content'];
 // Contact
 
 
-const contact_H4 = document.querySelector('.contact h4')
-contact_H4.innerText = siteContent['contact']['contact-h4'];
+const contact_h4 = document.querySelector('.contact h4');
+contact_h4.textContent = siteContent['contact']['contact-h4'];
 
 
 
 
-const contact_p = document.querySelector('.contact p')
-address.innerText = siteContent['contact']['address'];
-phone.innerText = siteContent['contact']['phone'];
-email.innerText = siteContent['contact']['email'];
+const contact_p = document.querySelectorAll('.contact p');
+contact_p[0].textContent = siteContent['contact']['address'];
+contact_p[1].textContent = siteContent['contact']['phone'];
+contact_p[2].textContent = siteContent['contact']['email'];
 
 //footer
-const footer = document.querySelector('footer p')
-footer.innerText = siteContent['footer']['copyright'];
+const footer = document.querySelector('footer p');
+footer.textContent = siteContent['footer']['copyright'];
 
- //let aTags = document.querySelectorAll("a");
-// let i;
-// for (i = 0; i < aTags.length; i++) {
-//   aTags[i].style.color = "red";
-// }
-
-// aTags.forEach(element => {
-//   element.style.color = 'blue';
-// });
+nav.forEach(nav => nav.style.color = 'green')
+navKid = document.createElement('a');
+navKid.textContent = '?';
+nav[5].appendChild(navKid);
 
